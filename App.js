b8e938a817c55573831b88fe2c3a11220e75bb71
supawadee khamnuengsithi <35362594+supawadee0789@users.noreply.github.com>
2021-05-11 20:37:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  product: productsReducer
+});
+const store = createStore(rootReducer);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
