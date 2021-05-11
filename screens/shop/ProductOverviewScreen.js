f3e3f285ac16e99054b45ca47bbcf56725024ff5
supawadee khamnuengsithi <35362594+supawadee0789@users.noreply.github.com>
2021-05-11 22:09:06 +0700

    navigation , redux  reducer

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import { FlatList, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
-const ProductsOverView = () =>{
-    return <FlatList/>
-}
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
 
-export default ProductsOverView;
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
+};
+
+export default ProductsOverviewScreen;
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,8 +2,9 @@ import PRODUCTS from '../../data/dummy-data';
 
 const initialState = {
     availableProducts: PRODUCTS,
-    userProducts: PRODUCTS.filter(prod.ownerId==='u1')
-}
-export default (state = initialState, action)=>{
+    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
+};
+
+export default (state = initialState, action) => {
     return state;
-}
+};
